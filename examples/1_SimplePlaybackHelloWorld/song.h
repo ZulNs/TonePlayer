8ec625dc34bfaaa1308a03abe5a6b11ddcfde6bf
ZulNs <zul.nasibu@gmail.com>
2023-02-21 18:09:09 +0800

    Update to version 2.0.0

--- examples/1_SimplePlaybackHelloWorld/song.h
+++ examples/1_SimplePlaybackHelloWorld/song.h
@@ -5,73 +5,74 @@
 
 
 // Song title: Hello World by Alan Walker ft. Torine
+#define BPM 104
+
 const PROGMEM uint8_t SONG[] = {
-  NAIS6, 8, NC7,   8, REST,  8, NCIS7, 2,
+  NAIS5, -4, NC6,   -4, REST,  -4, NCIS6, 8,
   // Address 8
-  NAIS6, 8, REST,  8, NCIS7, 4, NDIS7, 8, REST, 8, NC7,   2,
-  NAIS6, 8, NGIS6, 8, REST,  8, NAIS6, 2,
-  NGIS6, 8, REST,  8, NAIS6, 4, NCIS6, 8, REST, 8, NF6,   2,
-  NDIS6, 8, NCIS6, 8, REST,  8, NFIS6, 2,
-  NDIS6, 8, REST,  8, NFIS6, 4, NGIS6, 8, REST, 8, NF6,   2,
-  NDIS6, 8, NCIS6, 8, REST,  8, NDIS6, 2,
-  NCIS6, 8, REST,  8, NDIS6, 4, NGIS5, 8, REST, 8, NAIS5, 2,
-  REST,  2,
+  NAIS5, -4, REST,  -4, NCIS6, -2, NDIS6, -4, REST, -4, NC6,   8,
+  NAIS5, -4, NGIS5, -4, REST,  -4, NAIS5, 8,
+  NGIS5, -4, REST,  -4, NAIS5, -2, NCIS5, -4, REST, -4, NF5,   8,
+  NDIS5, -4, NCIS5, -4, REST,  -4, NFIS5, 8,
+  NDIS5, -4, REST,  -4, NFIS5, -2, NGIS5, -4, REST, -4, NF5,   8,
+  NDIS5, -4, NCIS5, -4, REST,  -4, NDIS5, 8,
+  NCIS5, -4, REST,  -4, NDIS5, -2, NGIS4, -4, REST, -4, NAIS4, 8,
+  REST,  -1,
   
   // Address 82
-  NAIS5,   4, NF6,    4, REST, 8, NF6,   0, 3, 4,
-  NDIS6,   4, NCIS6,  4, REST, 8,
-  // Address 98
-  NFIS6,   4, NFIS6,  4, REST, 8,
-  NGIS6,   4, NAIS6,  4, REST, 8, NF6,   0, 3, 4,
-  REPEAT, 98, 0,     16, 0,
-  NDIS6,   4, NCIS6,  4, REST, 8, NDIS6, 0, 3, 4, REST, 1,
+  NAIS4,   -2, NF5,    -2, REST, -4, NF5,   15,
+  NDIS5,   -2, NCIS5,  -2, REST, -4,
+  // Address 96
+  NFIS5,   -2, NFIS5,  -2, REST, -4,
+  NGIS5,   -2, NAIS5,  -2, REST, -4, NF5,   15,
+  REPEAT, 96, 0, 14, 0,
+  NDIS5,   -2, NCIS5,  -2, REST, -4, NDIS5, 15, REST, 20,
   
-  REPEAT, 82, 0, 49, 0,
+  REPEAT, 82, 0, 43, 0,
   
-  // Address 136
-  NAIS6,  4, NC7,   5, REST, 8, NCIS7, 2,
-  NAIS6,  4, NCIS7, 5, REST, 8, NC7,   2,
-  NAIS6,  4, NC7,   5, REST, 8, NAIS6, 2,
-  NGIS6,  4, NFIS6, 5, REST, 8, NF6,   2, REST, 2,
-  NFIS6,  2,
-  NDIS6,  4, NFIS6, 5, REST, 8, NF6,   2,
-  // Address 180
-  NDIS7, 16, NCIS6, 0, 3,   16, NCIS6, 5, REST, 8, NDIS6, 2,
-  NCIS6,  4, NC6,   5, REST, 8, NAIS5, 2, REST, 2,
+  // Address 130
+  NAIS5, -2, NC6,   -4, REST, -4, NCIS6, -1,
+  NAIS5, -2, NCIS6, -4, REST, -4, NC6,   -1,
+  NAIS5, -2, NC6,   -4, REST, -4, NAIS5, -1,
+  NGIS5, -2, NFIS5, -4, REST, -4, NF5,   -1, REST, -1,
+  NFIS5, -1,
+  NDIS5, -2, NFIS5, -4, REST, -4, NF5,   -1,
+  // Address 174
+  NDIS6, -8, NCIS5,  0, 3,     8, NCIS5, -4, REST, -4, NDIS5, -1,
+  NCIS5, -2, NC5,   -4, REST, -4, NAIS4, -1, REST, -1,
   
-  REPEAT, 142, 0, 60, 0,
+  REPEAT, 136, 0, 60, 0,
   
-  // Address 207
-  NF6,   8,
+  // Address 201
+  NF5, -4,
   REPEAT, 4, 0, 76, 0,
   
-  // Address 214
-  //NAIS5, 8, NCIS6, 8, NF6, 8, NAIS6, 8, NC7, 8, NCIS7, 8,
-  NAIS5, 8, NC6, 8, NCIS6, 8, NAIS6, 8, NC7, 8, NCIS7, 8,
+  // Address 208
+  NAIS4, -6, NC5, -6, NCIS5, -6, NAIS5, -6, NC6, -6, NCIS6, -6,
   REPEAT, 8, 0, 74, 0,
   
-  // Address 231
-  NAIS5, 4, NAIS5, 2, REST,  4,
-  NC6,   4, NC6,   2, REST,  4,
-  NCIS6, 4, NCIS6, 4, NDIS6, 8, NF6,   2, REST, 4,
-  NCIS6, 4, NFIS6, 4, NFIS6, 4,
-  NF6,   4, NFIS6, 4, NF6,   3,
-  NDIS6, 8, NCIS6, 4, NCIS6, 4, NDIS6, 2,
-  NCIS6, 4, NC6,   4, NAIS5, 2,
+  // Address 225
+  NAIS4, -2, NAIS4, -1, REST,  -2,
+  NC5,   -2, NC5,   -1, REST,  -2,
+  NCIS5, -2, NCIS5, -2, NDIS5, -4, NF5,   -1, REST, -4,
+  NCIS5, -2, NFIS5, -2, NFIS5, -2,
+  NF5,   -2, NFIS5, -2, NF5,   -2, REST,  -4,
+  NDIS5, -4, NCIS5, -2, NCIS5, -2, NDIS5, -1,
+  NCIS5, -2, NC5,   -2, NAIS4, -1,
   
-  REPEAT, 214, 0, 12, 0,
-  REPEAT, 231, 0, 48, 0,
+  REPEAT, 208, 0, 12, 0,
+  REPEAT, 225, 0, 50, 0,
   
-  REPEAT, 80, 0, 56, 0,
+  REPEAT, 80, 0, 50, 0,
   
-  // Address 294
-  NAIS6, 4, NC7,   4, NCIS7, 4, NC7,   4, NAIS6, 6, NAIS6, 3, REST, 2,
-  NDIS6, 3, REST,  3, NCIS6, 3, REST,  2,
-  NDIS6, 3, NCIS6, 3, NCIS6, 3, NCIS6, 3, NDIS6, 6, NF6,   2, REST, 3,
-  NDIS6, 3, NCIS6, 3, NCIS6, 3, NFIS6, 3, NGIS6, 6, NF6,   2, REST, 3,
-  NF7,   3, NDIS7, 3, NCIS7, 3, NCIS7, 3, NC7,   6, NAIS6, 2, REST, 3,
+  // Address 290
+  NAIS5, -4, NC6,   -4, REST,  -4, NCIS6, -4, NC6,   -4, REST,  -4, NAIS5, -4, NAIS5, -4, REST, -1,
+  NDIS5, -2, REST,  -2, NCIS5, -2, REST,  -2,
+  NDIS5, -2, NCIS5, -2, NCIS5, -2, NCIS5, -2, NDIS5, -2, NF5,   -1, REST,  -2,
+  NDIS5, -2, NCIS5, -2, NCIS5, -2, NFIS5, -2, NGIS5, -2, NF5,   -1, REST,  -2,
+  NF6,   -2, NDIS6, -2, NCIS6, -2, NCIS6, -2, NC6,   -2, NAIS5, -1, REST,  -2,
   
-  REPEAT, 136, 0, 95, 0,
+  REPEAT, 130, 0, 95, 0,
   
-  REPEAT, 80, 0, 56, 0
+  REPEAT, 80, 0, 50, 0
 };
